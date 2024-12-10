05beb18e217eddd1263256640d6e44b6515fa2af
judilegend <judilegend2@gmail.com>
2024-12-10 15:08:07 +0300

    refont : date pick en francais

--- src/components/sprints/SprintFilters.tsx
+++ src/components/sprints/SprintFilters.tsx
@@ -56,7 +56,7 @@ export function SprintFilters() {
             className="w-[240px] justify-start text-left font-normal"
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
-            {date ? format(date, "PPP") : <span>Pick a date</span>}
+            {date ? format(date, "PPP") : <span>Selectioné un date</span>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
