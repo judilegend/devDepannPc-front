c8f3efe0d2ba3199b07ae925c1befc3f118e2666
judilegend <judilegend2@gmail.com>
2025-01-12 16:07:16 +0300

    refont: cliend side dash

--- src/app/(backoffice)/dashboard/page.tsx
+++ src/app/(backoffice)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Suspense } from "react";
 import { ProjectList } from "@/components/projets/ProjectList";
 import { ProjectsHeader } from "@/components/projets/ProjectsHeader";
